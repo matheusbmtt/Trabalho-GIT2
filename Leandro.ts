aaddfc27318b793aacf7f4cb3d3f987dc629143e
LEANDRO HENRIQUE SANTOS GUARINIRI <04325839046@senacrs.edu.br>
2024-12-02 20:18:34 -0300

    LELEDo git Agora vaiiiii
    Boraaa vamo que dale

---
+++ Leandro.ts
@@ -0,0 +1,60 @@
+let ask = require('readline-sync')
+
+export interface Login{
+
+constructor()
+escolha()
+login()
+menu()
+cadastro()
+
+}
+
+
+export class Login implements Login{
+
+    cadastro(){}
+    menu(){
+        
+        console.log(`${console.clear()}                                       ---------------------------------------------------------------------\n
+                                                                        SEJA BEM VINDO\n
+                                                                              AO\n
+                                                                        BANCO BANRISUL\n
+                                           ---------------------------------------------------------------------
+                                           
+                                                                   FAÇA SEU LOGIN - 1
+                                                                   NOVO CADASTRO - 2`)
+    }
+
+    public escolha(){
+
+        let continua = true
+
+            while(continua){
+
+                this.menu()
+
+                let perguntaEscolha = ask.question('ESCOLHA UMA OPÇÃO: ')
+
+                switch(perguntaEscolha){
+
+                    case 1:
+                        this.login()
+                    break
+                    case 2: 
+                        this.cadastro()
+                    break
+
+                    default:
+                    console.log('escolha uma opção válida!')
+                }
+            }
+        
+        
+    
+
+
+}
+    }
+    
+
